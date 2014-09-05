1c04ca462d600a7fcda5258bb602b65fd63369ca
birme <jonas.birme@eyevinn.se>
2014-09-05 16:16:49 +0200

    Overriding HLS reader to force use of throttle proxy

--- app/core.js
+++ app/core.js
@@ -1,3 +1,27 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2014 Exceeds Your Expecations Vinn AB
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
 var eyevinnPlayer = angular.module('eyevinnplayer', []);
 var url = require('url');
 var request = require('request');
@@ -34,22 +58,13 @@ function mainController($scope, $http) {
 }
 
 function loadVideo(stream) {
-  var src = url.parse(stream);
-  var sdata = { "source": src };
   player.pause();
-  request.put(apithrottle, { form: sdata }, function(err, resp, body) {
-    var proxy = src;
-    proxy.host = "localhost:4000";
-    proxy.hostname = "localhost";  
-    var proxystream = url.format(proxy);
-    console.log(proxystream);
-    try {
-      player.src([ { type: "application/x-mpegURL", src: proxystream } ]);
-      player.play();
-    } catch (err) {
-      console.log("Error: " + err);
-    }
-  });    
+  try {
+    player.src([ { type: "application/x-mpegURL", src: stream } ]);
+    player.play();
+  } catch (err) {
+    console.log("Error: " + err);
+  }
 }
 
 function getSpeed(callback) {
